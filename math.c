726540715554808cd7f473d1120769e9ba7b70cb
Mohammed Jubair M <126316744+mohammedjubair-14@users.noreply.github.com>
2023-05-07 12:02:51 +0530

    Add files via upload

---
+++ math.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("\n SQUARE :%0.2f",sqrt(4));
+    printf("\n POWER  :%0.2f",pow(3,4));
+    printf("\n abs    ;%d",abs(-69));
+    printf("\n CEIL   :%f",ceil(4.8));
+    printf("\n CEIL   :%f",ceil(4.2));
+    printf("\n FLOOR  :%f",floor(4.8));
+    printf("\n FLOOR  :%f",floor(4.2));
+    printf("\n ROUND  :%f",round(3.2));
+    printf("\n ROUND  :%f",round(3.8));
+    return 0;
+}
